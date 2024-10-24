c1e01a7825ed863b9cd7ed8383e0875ebe96c2b7
Lukmanul Hakim <fank8192@gmail.com>
2024-10-24 20:50:07 +0700

    Add files via upload

---
+++ Percobaan 1.py
@@ -0,0 +1,7 @@
+harga = int(input("Harga barang:" ))
+jumlah = int (input ("jumlah barang:"))
+Total = jumlah*harga
+print("total", Total)
+Bayar = int (input("jumlah bayar: "))
+Kembalian = Bayar-Total
+print("kembalian", Kembalian)
